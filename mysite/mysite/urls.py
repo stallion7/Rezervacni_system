6c777603ff7573cc77590f67870e3542be2a6e39
stallion7 <stepankrautwurst@gmail.com>
2022-11-20 14:42:51 +0100

    1.6

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include
+
+from mysite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +12,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
