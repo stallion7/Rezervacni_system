7b574e876d0d43fc84f4182df7048484acef7083
stallion7 <stepankrautwurst@gmail.com>
2022-12-08 23:04:31 +0100

    2.5

--- mysite/slipka_gym/views.py
+++ mysite/slipka_gym/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, "uvod.html")
 
+@login_required
 def rezervace(request):
     return render(request, "rezervace.html")
 
+@login_required
 def uvod(request):
     return render(request, "uvod_prihlaseny.html")
